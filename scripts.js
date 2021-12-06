b9ee2236a63e65101c88e30f0bcb9cea56cbfaed
Nick Nenovski <nicknenovski@gmail.com>
2021-12-06 10:28:59 +0200

    JS part 1
    tab handling

--- scripts.js
+++ scripts.js
@@ -1,1 +1,34 @@
-console.log('it works')
+// Selectors
+const tabs = document.querySelector('.options');
+const styleHeader = document.querySelector('.options .style-header');
+const tabButtons = tabs.querySelectorAll('[role="tab"]');
+const stylePanels = tabs.querySelectorAll('[role="tabpanel"]');
+
+
+// Tab handling
+tabButtons.forEach(button => {
+    button.addEventListener('click', handleTabClick)
+})
+
+function handleTabClick(e) {
+    // Hide all Style options
+    stylePanels.forEach(panel => {
+        panel.hidden = true;
+    })
+
+    styleHeader.hidden = true;
+
+    // Unselect everything
+    tabButtons.forEach(tab => {
+        tab.setAttribute('aria-selected', false);
+    })
+    // Mark just the clicked option as selected
+    e.currentTarget.setAttribute('aria-selected', true);
+
+    // Find the associated Style option an show it
+    const { id } = e.currentTarget;
+    const stylePanel = tabs.querySelector(`[aria-labelledby="${id}"]`);
+    stylePanel.hidden = false;
+
+}
+
